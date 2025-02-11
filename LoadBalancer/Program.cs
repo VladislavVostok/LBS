558d1449c8ea9cdad336a4c2fc5df61ccd81dbfa
Vladislav East <vladislav.sage@yandex.ru>
2025-02-11 21:24:47 +0300

    Poker Server

--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -1,172 +1,183 @@
-﻿using System.IO;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LoadBalancer
 {
 
-    class Program
-    {
+	class Program
+	{
 
-        private static List<IPEndPoint> _servers = new List<IPEndPoint>{
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5001),
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5002),
-            new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5003)
-        };
+		private static List<IPEndPoint> _servers = new List<IPEndPoint>{
+			new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5001),
+			new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5002),
+			new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5003)
+		};
 
-        private static readonly object _lock = new object();
-        private static int _currentServerIndex = 0;
+		private static readonly object _lock = new object();
+		private static int _currentServerIndex = 0;
 
-        public static async Task Main(string[] args)
-        {
-            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
-            listener.Start();
+		private static readonly string _secretKey = "SuperSecretKeyForJWTTokensjdgfoasuet498u9gt8awujgre9q4yuthpaog;jawoiperytp123!";
 
-            Console.WriteLine("Балансировачный сервер запущен на порту 5000;");
 
-            while (true)
-            {
-                TcpClient client = await listener.AcceptTcpClientAsync();
-                _ = Task.Run(() => HandleClientAsync(client));
-            }
+		public static async Task Main(string[] args)
+		{
+			TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+			listener.Start();
+
+			Console.WriteLine("Балансировачный сервер запущен на порту 5000;");
+
+			while (true)
+			{
+				TcpClient client = await listener.AcceptTcpClientAsync();
+				_ = Task.Run(() => HandleClientAsync(client));
+			}
 
 
 
 		}
 
-        private static async Task HandleClientAsync(TcpClient client)
-        {
+		private static async Task HandleClientAsync(TcpClient client)
+		{
+			try
+			{
+				var cStream = client.GetStream();
+				byte[] buff = new byte[1024];
 
-            var cStream = client.GetStream();
-			byte[] buff = new byte[1024];
-			int byteRead = await cStream.ReadAsync(buff, 0, buff.Length);
-			string response = Encoding.UTF8.GetString(buff, 0, byteRead);
+				int bytesRead = await cStream.ReadAsync(buff, 0, buff.Length);
 
-            if (response.Trim() == "GET_SERVERS")
-            {
+				string request = Encoding.UTF8.GetString(buff, 0, bytesRead);
 
-                string res = "";
 
-                foreach (var s in _servers)
-                {
-                    res += s.ToString() + " | ";
-                }
 
-				byte[] re = Encoding.UTF8.GetBytes(res);
-				await cStream.WriteAsync(re, 0, re.Length);
+				if (!ValidateJwtToken(request))
+				{
+					Console.WriteLine("Неверный JWT токен. Отклоняем соединение.");
+					client.Dispose();
+					return;
+				}
 
+				IPEndPoint selectedServer = GetNextServer();
+
+				Console.WriteLine($"Клиент аутентифицирован. Перенаправляем на {selectedServer}");
+
+
+				//TODO: Если клиент был подключен его нужно адресовать на тотже сервер.
+				if (!await IsServerAvailableAsync(selectedServer))
+				{
+					Console.WriteLine($"Сервер {selectedServer} отдыхает. Пропускаем...");
+					return;
+				}
+
+				using (TcpClient server = new TcpClient())
+				{
+
+					await server.ConnectAsync(selectedServer);
+
+					await Task.WhenAll(
+						RedirectDataAsync(client.GetStream(), server.GetStream()),
+						RedirectDataAsync(server.GetStream(), client.GetStream())
+					);
+
+				}
 			}
-			else if(response.Trim().StartsWith("CONNET_TO") )
-
+			catch (Exception ex)
 			{
 
-                //IPEndPoint selectedServer = GetNextServer();
+				Console.WriteLine($"Error connecting to server: {ex.Message} - {ex.InnerException}");
 
-				IPEndPoint selectedServer = _servers[Convert.ToInt32(response.Trim().Split(" ")[1])];
-
-				byte[] re = Encoding.UTF8.GetBytes("OK");
-				await cStream.WriteAsync(re, 0, re.Length);
-
-				Console.WriteLine($"Клиент перенаправлен на севре {selectedServer}");
-
-
-
-                try
-                {
+			}
+			finally
+			{
+				client.Close();
+			}
+		}
 
 
 
+		private static async Task RedirectDataAsync(NetworkStream from, NetworkStream to)
+		{
+			byte[] buffer = new byte[1024];
+			int bytesRead;
 
-                //TODO: Если клиент был подключен его нужно адресовать на тотже сервер.
-                    if (!await IsServerAvailableAsync(selectedServer))
-                    {
-                        Console.WriteLine($"Сервер {selectedServer} отдыхает. Пропускаем...");
-                        return;
-                    }
+			try
+			{
 
-                    using (TcpClient server = new TcpClient())
-                    {
+				while ((bytesRead = await from.ReadAsync(buffer, 0, buffer.Length)) > 0)
 
-                        await server.ConnectAsync(selectedServer);
+				{
 
-                        await Task.WhenAll(
-                            RedirectDataAsync(client.GetStream(), server.GetStream()),
-                            RedirectDataAsync(server.GetStream(), client.GetStream())
-                        );
+					await to.WriteAsync(buffer, 0, bytesRead);
 
-                    }
-                }
-                catch (Exception ex)
-                {
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ошибка: {ex.Message} - {ex.InnerException}");
+			}
+			finally
+			{
+				await from.DisposeAsync();
+				await to.DisposeAsync();
+			}
+		}
 
-                    Console.WriteLine($"Error connecting to server: {ex.Message} - {ex.InnerException}");
+		private static async Task<bool> IsServerAvailableAsync(IPEndPoint server)
+		{
+			try
+			{
+				using (TcpClient pingClient = new TcpClient())
+				{
+					await pingClient.ConnectAsync(server.Address, server.Port);
+					Console.WriteLine($"Подключение к серверу {server} установлено.");
+				}
+				return true;
+			}
+			catch
+			{
 
-                }
-                finally
-                {
-                    client.Close();
-                }
-            }
+				return false;
+			}
+		}
 
-        }
+		private static IPEndPoint GetNextServer()
+		{
 
-        private static async Task RedirectDataAsync(NetworkStream from, NetworkStream to)
-        {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
+			lock (_lock)
+			{
+				IPEndPoint server = _servers[_currentServerIndex];
+				_currentServerIndex = (_currentServerIndex + 1) % _servers.Count;
+				return server;
+			}
+		}
 
-            try
-            {
+		private static bool ValidateJwtToken(string token)
+		{
+			try
+			{
+				var tokenHandler = new JwtSecurityTokenHandler();
+				var key = Encoding.UTF8.GetBytes(_secretKey);
 
-                while ((bytesRead = await from.ReadAsync(buffer, 0, buffer.Length)) > 0)
+				tokenHandler.ValidateToken(token, new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = false,
+					ValidateAudience = false
+				}, out SecurityToken validatedToken);
 
-                {
+				return validatedToken != null;
 
-                    await to.WriteAsync(buffer, 0, bytesRead);
+			}
+			catch
+			{
+				return false;
+			}
 
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.Message} - {ex.InnerException}");
-            }
-            finally
-            {
-                await from.DisposeAsync();
-                await to.DisposeAsync();
-            }
-        }
-
-        private static async Task<bool> IsServerAvailableAsync(IPEndPoint server)
-        {
-            try
-            {
-                using (TcpClient pingClient = new TcpClient())
-                {
-                    await pingClient.ConnectAsync(server.Address, server.Port);
-                    Console.WriteLine($"Подключение к серверу {server} установлено.");
-                }
-                return true;
-            }
-            catch
-            {
-
-                return false;
-            }
-        }
-
-        private static IPEndPoint GetNextServer()
-        {
-
-            lock (_lock)
-            {
-                IPEndPoint server = _servers[_currentServerIndex];
-                _currentServerIndex = (_currentServerIndex + 1) % _servers.Count;
-                return server;
-            }
-        }
-
-
-    }
+		}
+	}
 }
--- PokerClientCLI/Program.cs
+++ PokerClientCLI/Program.cs
@@ -2,124 +2,56 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 
 namespace PokerClientCLI
 {
-	internal class Program
+
+	class AuthResponse
 	{
+		public bool Success { get; set; }
+		public string Token { get; set; }
+	}
 
-		public class ServerEndPoint { 
-			public string Host { get; set; }
-			public int Port { get; set; }
 
-			public override string ToString()
-			{
-				return Host + ":" + Port;
-			}
-
-		}
-
-		static List<ServerEndPoint> EndPoints = new();
-
+	public class Program
+	{
+		private const string AuthServerIp = "127.0.0.1";
+		private const int AuthServerPort = 4000;
+		private const string BalancerIp = "127.0.0.1";
+		private const int BalancerPort = 5000;
 
 
 		static async Task Main(string[] args)
 		{
-
-			string serverIp = "127.0.0.1";
-			int serverPort = 5000; // Подключаемся к балансировщику
-
 			try
 			{
-				using TcpClient client = new TcpClient();
-				await client.ConnectAsync(serverIp, serverPort);
-				NetworkStream stream = client.GetStream();
+				Console.Write("Введите логин: ");
+				string username = Console.ReadLine();
 
-				while (true) {
+				Console.Write("Введите пароль: ");
+				string password = Console.ReadLine();
 
-					Console.WriteLine("Выберите пункт меню:\n" +
-						"1 - Получить список доступных серверов.\n" +
-						"2 - Подключиться к свыбранному серверу");
-					
-					int menu_item = Convert.ToInt32(Console.ReadLine());
+				string token = await GetAuthToken(username, password);
 
-					switch (menu_item){
-						case 1:
-							string request = "GET_SERVERS";
-							byte[] requestData = getRequestBytes(request);
-							await stream.WriteAsync(requestData, 0, requestData.Length);
-							
-							byte[] buff = new byte[1024];
-							int byteRead = await stream.ReadAsync(buff, 0, buff.Length);
-							
-							string response = Encoding.UTF8.GetString(buff, 0, byteRead);
-
-							string[] servers = response.Split(" | ");
-
-							foreach(string s in servers)
-							{
-								EndPoints.Add(new ServerEndPoint
-								{
-									Host = s.Trim().Split(":")[0],
-									Port = Convert.ToInt32(s.Trim().Split(":")[1])
-
-								});
-							}
-
-
-							for (int i = 0; i < EndPoints.Count; i++)
-							{
-								Console.WriteLine($"{i + 1} -> {EndPoints[i].ToString()}");
-							}
-
-
-
-
-							break;
-						case 2:
-
-							Console.WriteLine("Введите номер сервера: ");
-							int conSer = Convert.ToInt32(Console.ReadLine());
-
-							request = "CONNET_TO " + conSer.ToString();
-							requestData = getRequestBytes(request);
-							await stream.WriteAsync(requestData, 0, requestData.Length);
-							
-							buff = new byte[1024];
-							byteRead = await stream.ReadAsync(buff, 0, buff.Length);
-							
-		
-							response = Encoding.UTF8.GetString(buff, 0, byteRead);
-
-
-							if (response == "OK")
-							{
-
-								Console.WriteLine("Сервер готов!");
-
-
-							}
-							else
-							{
-								Console.WriteLine("Выберите другой!");
-							}
-
-							break;
-						case 3: break;
-						case 4: break;
-					}
-
-
-
-
-
+				if (string.IsNullOrEmpty(token))
+				{
+					Console.WriteLine("Ошибка авторизации. Завершаем работу.");
+					return;
 				}
+
+				Console.WriteLine($"Токен получен: {token}\n");
+
+
+				await ConnectToBalancer(token);
 
 
 			}
-			catch { 
-			
-			
+			catch (SocketException e)
+			{
+
+				Console.WriteLine($"{e.Message} - {e.InnerException}");
+
 			}
 		}
 
@@ -129,5 +61,70 @@ namespace PokerClientCLI
 
 		}
 
+
+		private static async Task ConnectToBalancer(string token)
+		{
+			try {
+				using TcpClient client = new TcpClient();
+				await client.ConnectAsync(BalancerIp, BalancerPort);
+				NetworkStream stream = client.GetStream();
+
+				byte[] tokenData = Encoding.UTF8.GetBytes(token);
+				await stream.WriteAsync(tokenData, 0, tokenData.Length);
+				byte[] buffer = new byte[1024];
+
+				int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+				string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+				Console.WriteLine($"Ответ от сервера: {response}");
+
+				Console.Write("Введите Команду: ");
+				string command = Console.ReadLine();
+
+				byte[] commandByte = Encoding.UTF8.GetBytes(token);
+				await stream.WriteAsync(commandByte, 0, tokenData.Length);
+				buffer = new byte[1024];
+
+				bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+				response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+				Console.WriteLine($"Ответ от сервера: {response}");
+			}
+			catch (Exception ex)
+			{
+
+				Console.WriteLine($"Ошибка подключения к балансировщику: {ex.Message}");
+			}
+		}
+
+		private static async Task<string> GetAuthToken(string username, string password)
+		{
+
+			try
+			{
+				using TcpClient client = new TcpClient();
+				await client.ConnectAsync(AuthServerIp, AuthServerPort);
+				NetworkStream stream = client.GetStream();
+
+				var loginRequest = new { Username = username, Password = password };
+				string jsonRequest = JsonSerializer.Serialize(loginRequest);
+				byte[] requestData = Encoding.UTF8.GetBytes(jsonRequest);
+
+				await stream.WriteAsync(requestData, 0, requestData.Length);
+				byte[] buffer = new byte[1024];
+
+				int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+				string responseJson = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+				var response = JsonSerializer.Deserialize<AuthResponse>(responseJson);
+				Console.WriteLine($"Ответ от сервера: {response}");
+				return response.Token.ToString();
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ошибка подключения к балансировщику: {ex.Message}");
+			}
+
+			return string.Empty;
+		}
 	}
 }
